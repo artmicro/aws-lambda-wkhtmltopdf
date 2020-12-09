4042fa1c10c588d04cbd71577b980065d1bf9719
Andrei Badea <andrei.badea@liftago.com>
2020-12-09 16:14:36 +0100

    LTG-11916: Make Soleil available

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM amazonlinux:2
 RUN yum install -y gcc-c++ make
 RUN curl -sL https://rpm.nodesource.com/setup_12.x | bash -
 RUN yum install -y nodejs
+# This is so we can list the available fonts via
+# FONTCONFIG_PATH=/var/task/fonts fc-list
+RUN yum install -y fontconfig
 
 VOLUME /var/task
 ENV LAMBDA_TASK_ROOT=/var/task
--- test.js
+++ test.js
@@ -6,6 +6,8 @@ const index = require("./index.js");
 
 const body = {
     url: "https://admin.liftago.com/emails/attachments/receipts/gPW5XRhJqpQzO0BS/1005753771",
+    // Another one (but note the token has limited validity so it may have expired already):
+    // https://daruj.liftago.com/gift-voucher?t=MjE4NjQzNHwxNjA3NTg5NTY3ODQwfExOWEpoUVRPakotV1VUY3Vpdm0zMkJHXzlJYz0=
     options: {
       "pageSize": "A4",
       "orientation": "Portrait"
